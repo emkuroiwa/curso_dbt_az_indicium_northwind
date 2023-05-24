5cc7658a054b0f320e6de70a3fb2f75af9ec2259
emy.engfisica@gmail.com <emy.engfisica@gmail.com>
2023-05-24 00:44:54 +0000

    Creating marts folder

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -1,8 +1,14 @@
 with
-    orders as (
-        select *
+    customers as (
+        select
+            customer_id
+            , company_name
+            , contact_name
+            , address
+            , city
+            , country
         from {{ source('northwind', 'customers')}}
     )
 
 select *
-from orders
+from customers
